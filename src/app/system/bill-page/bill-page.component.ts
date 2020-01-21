8749a0562cd719cf4464bb8a031c6b5dd47a66b8
VitaliyBaboshin <vitaliy2@ro.ru>
2020-01-22 00:20:56 +0500

    add

---
+++ src/app/system/bill-page/bill-page.component.ts
@@ -0,0 +1,47 @@
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {BillService} from '../shared/services/bill.service';
+import {combineLatest, Subscription} from 'rxjs';
+import {Bill} from '../shared/models/bill.model';
+
+@Component({
+  selector: 'wfm-bill-page',
+  templateUrl: './bill-page.component.html',
+  styleUrls: ['./bill-page.component.scss']
+})
+export class BillPageComponent implements OnInit, OnDestroy {
+  sub1: Subscription;
+  sub2: Subscription;
+
+  currency: any;
+  bill: Bill;
+  isLoaded = false;
+
+  constructor(public billService: BillService) { }
+  ngOnInit() {
+        this.sub1 = combineLatest([
+          this.billService.getBill(),
+          this.billService.getCurrency()
+        ])
+          .subscribe((data: [Bill, any]) => {
+            this.bill = data[0];
+            this.currency = data[1];
+            this.isLoaded = true;
+            // console.log(data);
+            // console.log(this.currency);
+          });
+  }
+  onRefresh() {
+    this.isLoaded = false;
+    this.sub2 = this.billService.getCurrency()
+      .subscribe((currency: any) => {
+      this.currency = currency;
+      this.isLoaded = true;
+    });
+  }
+  ngOnDestroy() {
+    this.sub1.unsubscribe();
+    if (this.sub2) {
+      this.sub2.unsubscribe();
+    }
+  }
+}
