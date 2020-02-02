3e119844ef1a42bbb5377e8f9f312a39f0e96397
VitaliyBaboshin <vitaliy2@ro.ru>
2020-02-02 22:49:17 +0500

    +

--- src/app/system/shared/services/bill.service.ts
+++ src/app/system/shared/services/bill.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Bill} from '../models/bill.model';
-import {map} from 'rxjs/operators';
+import {delay, map} from 'rxjs/operators';
 import {BaseApi} from '../../../shared/core/base-api';
 
 @Injectable()
@@ -10,6 +10,9 @@ export class BillService extends BaseApi {
   constructor(public http: HttpClient) {super(http); }
   getBill(): Observable<Bill> {
     return this.get('bill');
+  }
+  updateBill(bill: Bill): Observable<Bill> {
+    return this.put('bill', bill);
   }
   getCurrency(base: string = 'RUB'): Observable<any> {
     return this.http.get(`https://api.ratesapi.io/api/latest?base=${base}`)
---
+++ src/app/system/shared/services/events.service.ts
@@ -0,0 +1,20 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+import {BaseApi} from '../../../shared/core/base-api';
+import {WFMEvent} from '../models/event.model';
+@Injectable()
+export class EventsService extends BaseApi {
+  constructor(public http: HttpClient) {
+    super(http);
+  }
+
+  addEvent(event: WFMEvent): Observable<WFMEvent> {
+    return this.post('events', event);
+  }
+
+  getEvents(): Observable<WFMEvent[]> {
+    return this.get('events');
+  }
+}
