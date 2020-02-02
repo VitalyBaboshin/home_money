379b72583ddd1202a6bb9640669b5a19c5569cb6
VitaliyBaboshin <vitaliy2@ro.ru>
2020-02-02 22:48:29 +0500

    +AuthGuard

--- src/app/system/system-routing.module.ts
+++ src/app/system/system-routing.module.ts
@@ -5,19 +5,19 @@ import {BillPageComponent} from './bill-page/bill-page.component';
 import {HistoryPageComponent} from './history-page/history-page.component';
 import {PlanningPageComponent} from './planning-page/planning-page.component';
 import {RecordsPageComponent} from './records-page/records-page.component';
+import {AuthGuard} from '../shared/services/auth.guard';
 
 const routes: Routes = [
-  {path: 'system', component: SystemComponent, children: [
+  {path: '', component: SystemComponent, canActivate: [AuthGuard], children: [
       {path: 'bill', component: BillPageComponent},
       {path: 'history', component: HistoryPageComponent},
       {path: 'planning', component: PlanningPageComponent},
       {path: 'records', component: RecordsPageComponent}
     ]}
-]
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class SystemRoutingModule{
-}
+export class SystemRoutingModule {}
