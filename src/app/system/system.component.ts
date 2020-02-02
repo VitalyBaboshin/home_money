1485a375472ae0c61efe40a600b9e4e3170e3c1d
VitaliyBaboshin <vitaliy2@ro.ru>
2020-02-02 22:49:30 +0500

    +

--- src/app/system/system.component.ts
+++ src/app/system/system.component.ts
@@ -1,9 +1,13 @@
-import {Component} from '@angular/core';
+import {Component, HostBinding} from '@angular/core';
+import {fadeStateTrigger} from '../shared/animations/fade.animation';
 
 @Component({
   selector: 'wfm-system',
-  templateUrl: './system.component.html'
+  templateUrl: './system.component.html',
+  animations: [fadeStateTrigger]
 })
 export class SystemComponent {
-
+  // байндим определенное свойств на элементе
+  // прикрепляем анимацию с помощью знака @
+  @HostBinding('@fade') a = true;
 }
