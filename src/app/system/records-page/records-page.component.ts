612379df08e86fe80c80ae559d51feec4d822adc
VitaliyBaboshin <vitaliy2@ro.ru>
2020-02-02 22:49:46 +0500

    +

--- src/app/system/records-page/records-page.component.ts
+++ src/app/system/records-page/records-page.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {Title} from '@angular/platform-browser';
+
+import {Category} from '../shared/models/category.model';
+import {CategoriesService} from '../shared/services/categories.service';
+
 
 @Component({
   selector: 'wfm-records-page',
@@ -6,10 +11,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./records-page.component.scss']
 })
 export class RecordsPageComponent implements OnInit {
+  categories: Category[] = [];
+  isLoaded = false;
 
-  constructor() { }
+  constructor(private categoriesService: CategoriesService,
+              private title: Title
+              ) {
+    title.setTitle('Страница записи');
+  }
 
   ngOnInit() {
+    this.categoriesService.getCategories()
+      .subscribe((categories) => {
+        this.categories = categories;
+        this.isLoaded = true;
+      });
+  }
+  newCategoryAdded(category: Category) {
+    this.categories.push(category);
+  }
+
+  categoryWasEdited(category: Category) {
+    const idx = this.categories
+      .findIndex(c => c.id === category.id);
+    this.categories[idx] = category;
   }
 
 }
