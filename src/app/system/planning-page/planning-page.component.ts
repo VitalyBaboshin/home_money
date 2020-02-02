11bc02191eb7bab40e4e129e7b26410577df5c8f
VitaliyBaboshin <vitaliy2@ro.ru>
2020-02-02 22:46:35 +0500

    update planing-page

--- src/app/system/planning-page/planning-page.component.ts
+++ src/app/system/planning-page/planning-page.component.ts
@@ -1,15 +1,75 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {BillService} from '../shared/services/bill.service';
+import {CategoriesService} from '../shared/services/categories.service';
+import {EventsService} from '../shared/services/events.service';
+import {combineLatest, Subscription} from 'rxjs';
+import {Title} from '@angular/platform-browser';
+
+import {Bill} from '../shared/models/bill.model';
+import {Category} from '../shared/models/category.model';
+import {WFMEvent} from '../shared/models/event.model';
 
 @Component({
   selector: 'wfm-planning-page',
   templateUrl: './planning-page.component.html',
   styleUrls: ['./planning-page.component.scss']
 })
-export class PlanningPageComponent implements OnInit {
+export class PlanningPageComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  isLoaded = false;
+  sub1: Subscription;
+
+  bill: Bill;
+  categories: Category[];
+  events: WFMEvent[];
+
+  constructor(private billService: BillService,
+              private categoriesService: CategoriesService,
+              private eventsService: EventsService,
+              private title: Title) {
+    title.setTitle('Страница планирования');
+  }
 
   ngOnInit() {
+    this.sub1 = combineLatest([
+      this.billService.getBill(),
+      this.categoriesService.getCategories(),
+      this.eventsService.getEvents()
+    ]).subscribe((data: [Bill, Category[], WFMEvent[]]) => {
+      this.bill = data[0];
+      this.categories = data[1];
+      this.events = data[2];
+      this.isLoaded = true;
+    });
+  }
+
+  getCategoryCost(cat: Category): number {
+    const catEvents = this.events.filter(e => e.category === cat.id && e.type === 'outcome');
+    return catEvents.reduce((total, e) => {
+      total += e.amount;
+      return total;
+    }, 0);
+  }
+
+  private getPercent(cat: Category): number {
+    const percent = (100 * this.getCategoryCost(cat)) / cat.capacity;
+    return percent > 100 ? 100 : percent;
+  }
+
+  getCatPercent(cat: Category): string {
+    return this.getPercent(cat) + '%';
+  }
+
+  getCatColorClass(cat: Category): string {
+    const percent = this.getPercent(cat);
+    return percent < 60 ? 'success' : percent >= 100 ? 'danger' : 'warning';
+  }
+
+
+  ngOnDestroy() {
+    if (this.sub1) {
+      this.sub1.unsubscribe();
+    }
   }
 
 }
