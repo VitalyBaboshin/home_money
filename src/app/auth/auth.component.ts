b8e11071e3b1425fd576ceb7da59ac682e86f620
VitaliyBaboshin <vitaliy2@ro.ru>
2020-02-02 22:38:51 +0500

    add animations

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,12 +1,16 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, HostBinding, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
+import {fadeStateTrigger} from '../shared/animations/fade.animation';
 
 @Component({
   selector: 'wfm-auth',
-  templateUrl: './auth.component.html'
+  templateUrl: './auth.component.html',
+  animations: [fadeStateTrigger]
 })
- export class AuthComponent implements OnInit{
-
+ export class AuthComponent implements OnInit {
+  // байндим определенное свойств на элементе
+  // прикрепляем анимацию с помощью знака @
+  @HostBinding('@fade') a = true;
   constructor(private router: Router) {}
 
   ngOnInit() {
